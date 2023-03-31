12fcae669a52a83c63de6c2c78fa99fb0bbc86fc
Shriganesh Bhide <shriganesh2019@iiitkottayam.ac.in>
2023-03-31 09:08:18 +0530

    model prototype created.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class offset(models.Model):
+    #id = models.AutoField(primary_key=True)
+    store_id = models.CharField(primary_key = True , max_length=30)
+    loc = models.TextField()
+    name = models.TextField()
+    abbrevation = models.TextField()
+    offset = models.DurationField()
+    is_dst = models.BooleanField()
+    
+    
+    class Meta:
+        db_table = 'id_offset'
+        # managed = True
+        # verbose_name = 'ModelName'
+        # verbose_name_plural = 'ModelNames'
